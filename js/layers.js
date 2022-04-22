37d37893ab04ddb44c8287e6fc47c33d16a9f4e3
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2022-04-22 15:02:21 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,7 +8,7 @@ addLayer("D", {
     }},
     color: "#AFEEEE",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "Data", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -46,5 +46,40 @@ tooltip: "+3/s",
 			},
 	},
 	layerShown(){return true},
-	
 })
+
+addLayer("SD", {
+    name: "SD", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "SD", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 10, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#AFEEEE",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "DeepData", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal",	
+	branches: ["D"],// normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.4, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        let exp = new Decimal(1)
+		return exp;
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+	layerShown(){return true},
+})
+
+
+
+
+
