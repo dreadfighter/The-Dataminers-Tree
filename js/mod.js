c7069b5e2b6de07f57894541fd044c92a653bc86
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2022-04-23 17:54:26 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.3b",
+	name: "The Dataminers TreeBeta",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
