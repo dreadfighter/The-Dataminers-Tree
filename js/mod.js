33ba1231c11fe54c19d5eb98817b1206695c6e67
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2022-05-23 23:23:19 +0300

    0.1b

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The Dataminers Tree",
-	id: "dataminer",
+	name: "The Chemistry Lab",
+	id: "atom",
 	author: "Seder3214",
-	pointsName: "miners",
+	pointsName: "Atoms",
 	modFiles: ["layers.js", "tree.js"],
 	endgame: new Decimal("1e388"),
 
@@ -14,8 +14,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.3b",
-	name: "The Dataminers TreeBeta",
+	num: "0.1b",
+	name: "The Chemistry LabBeta",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
@@ -44,71 +44,9 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	// Data Upgrades (total: 15)
-		if (hasUpgrade("D", 11)) gain = gain.times(2);
-		if (hasUpgrade("D", 12)) gain = gain.times(1.7);
-		if (hasUpgrade("D", 13)) gain = gain.times(3);
-		if (hasUpgrade("D", 14)) gain = gain.times(2.5);
-		if (hasUpgrade("D", 15)) gain = gain.times(3.25);
-
-		 if (hasUpgrade("D", 21)) gain = gain.times(2);
-		 if (hasUpgrade("D", 22)) gain = gain.times(2);
-		 if (hasUpgrade("D", 23)) gain = gain.times(1.5);
-		 if (hasUpgrade("D", 31)) gain = gain.times(1.4);
-		 if (hasUpgrade("D", 32)) gain = gain.times(1.6);
-		 if (hasUpgrade("D", 41)) gain = gain.times(2.75);
-		 if (hasUpgrade("D", 51)) gain = gain.times(1.5);
-		 if (hasUpgrade("D", 52)) gain = gain.times(1.25);
-		 if (hasUpgrade("D", 53)) gain = gain.times(1.4);
-		 if (hasUpgrade("D", 54)) gain = gain.times(1.25);
-    // Singularity Data upgrades (total: 3)
-		  if (hasUpgrade("SD", 12)) gain = gain.times(1.8);
-		  if (hasUpgrade("SD", 21)) gain = gain.times(1.7);
-	      if (hasUpgrade("SD", 11)) gain = gain.times(2);
-    // Deep Data upgrades (total: 2)
-		   if (hasUpgrade("DD", 11)) gain = gain.times(3);
-           if (hasUpgrade("DD", 12)) gain = gain.times(4);
-    // Tier 1 Data upgrades (total: 5)
-		    if (hasUpgrade("t1", 11)) gain = gain.times(3);
-			if (hasUpgrade("t1", 12)) gain = gain.times(1.75);
-			if (hasUpgrade("t1", 21)) gain = gain.times(1.3);
-			if (hasUpgrade("t1", 22)) gain = gain.times(1.6);
-			if (hasUpgrade("t1", 23)) gain = gain.times(1.9);
-    // Tier 1+ Data upgrades (total: 4)
-			 if (hasUpgrade("t11", 11)) gain = gain.times(1.85);
-			 if (hasUpgrade("t11", 12)) gain = gain.times(1.7);
-			 if (hasUpgrade("t11", 21)) gain = gain.times(2);
-			 if (hasUpgrade("t11", 32)) gain = gain.times(4);
-    // Tier 2 Data upgrades (total: 4)
-			  if (hasUpgrade("t2", 13)) gain = gain.times(2.25)	
-              if (hasUpgrade("t2", 11)) gain = gain.times(3);
-              if (hasUpgrade("t2", 21)) gain = gain.times(1.5);
-               if (hasUpgrade("t2", 22)) gain = gain.times(1.7);			  
-    // Tier 2+ Data upgrades (total: 3)		  
-               if (hasUpgrade("t22", 11)) gain = gain.times(1.4);
-               if (hasMilestone("t22", 2)) gain = gain.times(2);
-               if (hasUpgrade("t22", 12)) gain = gain.times(1.5);
-	// Planetary Data upgrades (total: 3)
-	           if (hasUpgrade("P", 11)) gain = gain.times(1.6);
-			   if (hasUpgrade("P", 12)) gain = gain.times(1.4);
-			   if (hasUpgrade("P", 13)) gain = gain.times(1.5);
-	// Singularity upgrades (total: 4)
-	           if (hasUpgrade("S", 11)) gain = gain.times(2);
-			   if (hasUpgrade("S", 12)) gain = gain.times(2);
-			   if (hasUpgrade("S", 13)) gain = gain.times(2);
-			   if (hasUpgrade("S", 15)) gain = gain.times(11);
-    // Tier 1 Mega upgrades (total: 1)
-	           if (hasUpgrade("T1M", 11)) gain = gain.times(1.3);
-			   if (hasUpgrade("T1M", 12)) gain = gain.times(1.1);
-			   if (hasUpgrade("T1M", 13)) gain = gain.times(1.1);
-			   if (hasUpgrade("T1M", 21)) gain = gain.times(1.2);
-			   if (hasUpgrade("T1M", 22)) gain = gain.times(1.15);
-			   if (hasUpgrade("T1M", 23)) gain = gain.times(1.15);
-	// Galaxy Data upgrades (total: 1)
-	           if (hasUpgrade("G", 11)) gain = gain.times(2);	
-               if (hasUpgrade("G", 12)) gain = gain.times(2);	
-               if (hasUpgrade("G", 21)) gain = gain.times(3);
-if (hasUpgrade("G", 22)) gain = gain.times(3);			   
+if (hasUpgrade("H", 11)) gain = gain.times(1.7);
+if (hasUpgrade("H", 12)) gain = gain.times(1.85);
+if (hasUpgrade("H", 21)) gain = gain.times(1.6);		
 	return gain
 }
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
