45fee4a2f4efd5d128b1d0fb5fe65758c08d6d42
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2022-07-27 16:06:59 +0300

    0.2.2

--- js/layers.js
+++ js/layers.js
@@ -6,6 +6,7 @@ addLayer("cp", {
         unlocked: true,
 		points: new Decimal(0),
 		power: new Decimal(0),
+		gainy: new Decimal(1),
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
@@ -18,7 +19,9 @@ return "Endgame: 128th Challenge, each 5 of Challenge Points forming a challenge
     exponent: 0.55, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-		if (inChallenge("dr", 11)) mult = mult.div(1.74) 
+		if (inChallenge("dr", 11)) mult = mult.div(1.74)
+		if (inChallenge("dr", 12)) mult = mult.div(2.36)	
+		if (inChallenge("dr", 13)) mult = mult.div(2.84)			
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -61,13 +64,17 @@ addLayer("dr", {
     symbol: "DR", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
+		power: new Decimal(0),
     }},
     color: "#523e8b",
     requires: new Decimal(100), // Can be a function that takes requirement increases into account
     resource: "Dimensional Rift",	// Name of prestige currency
     baseResource: "challenge points",
+	effectDescription() {
+		return " which provides " + format(player.dr.power) + " Power, which gains " + format(player.dr.power.pow(0.25)) + "x to point gain" 
+	},
 branches: ["cp"],	// Name of resource prestige is based on
     baseAmount() {return player.cp.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -84,45 +91,95 @@ branches: ["cp"],	// Name of resource prestige is based on
         name: "4. 1st dimension",
         challengeDescription: "CP gain is 1.74x slower",
         canComplete: function() {return player.cp.points.gte(85)},
-		unlocked() {if (hasChallenge('dr', 21)) return false
+		unlocked() {
 			return true},
 		goalDescription: " 85 Challenge Points",
 		rewardDescription: "Unlock 2 challenges, which can unlock one more layer",
     },
 	    12: {
         name: "5. 2nd dimension",
-        challengeDescription: "CP gain is 1.74x slower and point gain is 1.24x slower",
+        challengeDescription: "CP gain is 2.36x slower and point gain is 1.27x slower",
         canComplete: function() {return player.cp.points.gte(155)},
-		unlocked() {if (hasChallenge('dr', 21)) return false
-			return hasChallenge("dr", 11)},
+		unlocked() {return hasChallenge("dr", 11)},
 		goalDescription: " 155 Challenge Points",
 		rewardDescription: "Unlock challenge and provide 2.15x boost to point gain",
     },
-		    12: {
-        name: "6. 3nd dimension",
-        challengeDescription: "",
-        canComplete: function() {return player.cp.points.gte(155)},
-		unlocked() {if (hasChallenge('dr', 21)) return false
+		    13: {
+        name: "6. 3rd dimension",
+        challengeDescription: "CP gain is 2.84x slower and point gain is 1.35x slower",
+        canComplete: function() {return player.cp.points.gte(265)},
+		unlocked() {
 			return hasChallenge("dr", 12)},
-		goalDescription: " 155 Challenge Points",
-		rewardDescription: "Unlock next challenge and provide <b>Power</b>",
+		goalDescription: " 265 Challenge Points",
+		rewardDescription: "Unlock next challenge and upgrade <b>Power</b> effect",
     },
 		    21: {
         name: "13. Collapse all dimensions",
         challengeDescription: "Just click at this challenge",
         canComplete: function() {return player.cp.points.gte(0)},
-		unlocked() {return hasChallenge("dr", 11)},
+		unlocked() {return (hasChallenge("dr", 13))},
 		goalDescription: " 0 Challenge Points",
 		rewardDescription: "Unlock a new layer, but destroy this layer",
     },
 },
 update(diff) {
-	let player.dr.power = player.dr.power.add(diff)
+	if (player.dr.unlocked) return player.dr.power = player.dr.power.add(diff)
 },
     row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "c", description: "c: Reset for Challenge Points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "d", description: "d: Reset for Dimensional Rift", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-	layerShown(){if (hasChallenge('dr', 21)) return "ghost"
-		return hasChallenge("cp", 13) || player[this.layer].unlocked},
+	layerShown(){if (hasChallenge('dr', 21) || player.e.unlocked) return "ghost"
+		return (hasChallenge("cp", 13) || player[this.layer].unlocked)},
+})
+addLayer("e", {
+    name: "Emptiness", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#484d5b",
+    requires: new Decimal(1000), // Can be a function that takes requirement increases into account
+    resource: "Emptiness",	// Name of prestige currency
+    baseResource: "challenge points",
+branches: ["cp"],	// Name of resource prestige is based on
+    baseAmount() {return player.cp.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.31, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+	challenges: {
+		    11: {
+        name: "14. Collect dust",
+        challengeDescription: "There are nothing that can slow down all production",
+        canComplete: function() {return player.cp.points.gte(1000)},
+		unlocked() {
+			return true},
+		goalDescription: " 1000 Challenge Points",
+		rewardDescription: "Unlock an <b>Expedition</b> challenge",
+    },
+			    12: {
+        name: "15. Expedition",
+		completionLimit: 10,
+        challengeDescription() { let comps = this.completionLimit
+			return "Gather Sand. Completed " + format(challengeCompletions("e", 12)) + " / "+ format(comps)},
+        canComplete: function() {return player.cp.points.gte(2000)},
+		unlocked() {
+			return true},
+		goalDescription: " 2000 Challenge Points",
+		rewardDescription: "Unlock an <b>Expedition</b> challenge",
+    },
+	},
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "e", description: "e: Reset for Emptiness", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+	layerShown(){return (hasChallenge("dr", 13) || player[this.layer].unlocked)},
 })
--- js/mod.js
+++ js/mod.js
@@ -1,10 +1,10 @@
 let modInfo = {
-	name: "The Chemistry Lab",
-	id: "atom",
+	name: "The Challenge Tree",
+	id: "particles",
 	author: "Seder3214",
 	pointsName: "Particles",
 	modFiles: ["layers.js", "tree.js"],
-	endgame: new Decimal("1e388"),
+	endgame: new Decimal("1e15000"),
 
 	discordName: "",
 	discordLink: "",
@@ -14,16 +14,24 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1b",
-	name: "The Chemistry LabBeta",
+	num: "0.2",
+	name: "The Challenge Tree: Emptiness Update",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Added Challenge Points Layer<br>
+		- Added some challenges<br>
+			<h3>v0.1.5</h3><br>
+		- Added Dimensional Rift Layer<br>
+		- Added more challenges<br>
+					<h3>v0.2</h3><br>
+		- Added Emptiness Layer<br>
+		- Added 2 challenges`
+		
+		
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You have competed all the challenges up to 128th!`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
@@ -42,13 +50,16 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-		let gain = new Decimal(0)
-if (player.cp.points.gte(1)) gain = gain.plus(1)		
+		let gain = new Decimal(1)	
 if (inChallenge("cp", 11)) gain = gain.times(0.8)
 if (hasChallenge("cp", 11)) gain = gain.times(20.15)
 if (inChallenge("cp", 12)) gain = gain.div(player.cp.points.div(1.23))	
 if (hasChallenge("cp", 12)) gain = gain.times(player.points.pow(0.24)).add(1)
 if (inChallenge("cp", 13)) gain = gain.div(2)
+	if (inChallenge("dr", 12)) gain = gain.div(1.27)
+	if (player.dr.unlocked) gain = gain.times(player.dr.power.pow(0.25))
+		if (hasChallenge("dr", 12)) gain = gain.times(2.15)
+				if (inChallenge("dr", 13)) gain = gain.div(1.34)
 	return gain
 }
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
