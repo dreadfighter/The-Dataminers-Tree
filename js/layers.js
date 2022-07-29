952d57c314ce3dd171ff07188c47e2356ee8e820
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2022-07-29 20:40:38 +0300

    0.75

--- js/layers.js
+++ js/layers.js
@@ -651,5 +651,5 @@ layerDataReset("mf")
 layerDataReset("mg")
 		},
 	layerShown(){let r = challengeCompletions("e",12)
-		return (hasMilestone("mf", 11) || player[this.layer].unlocked)},
+		return (hasMilestone("cp", 32) || player[this.layer].unlocked)},
 })
